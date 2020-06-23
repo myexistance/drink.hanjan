f7e8a0591ef603422644a203f525f3b4dac76fea
myexistance <joonyoonlives@gmail.com>
2020-06-23 16:04:21 +1200

    React Typed added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+import Typed from "react-typed";
 import Toolbar from './components/Toolbar/Toolbar'
 import SideDrawer from './components/SideDrawer/SideDrawer'
 import Backdrop from './components/Backdrop/Backdrop'
+import Portfolio from './components/portfolio'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css'
 
 class App extends Component {
   state = {
@@ -30,9 +36,43 @@ class App extends Component {
         <SideDrawer show={this.state.sideDrawerOpen} />
         {Backdrop}
         <main style={{margintTop: '64px'}}>
-          <p> This is the content</p>
+        <div id="home" className="intro route bg-image background">
+        <div className="intro-content">
+
+            <div className="container">
+              <h1 className="intro-title">My name is Joon</h1>
+              <p className="intro-subtitle">
+                <span className="text-slider-items"></span>
+                <strong className="text-slider">
+                  <Typed
+                    strings={[
+                      "Front End Developer",
+                      "Painter",
+                      "Software Engineer"
+                    ]}
+                    typeSpeed={80}
+                    backDelay={1100}
+                    backSpeed={30}
+                    loop
+                  />
+                </strong>
+              </p>
+              <p className="pt-4">
+                <a
+                  className="btn btn-primary btn px-12"
+                  to="/portfolio"
+                  role="button"
+                >
+                  View My Work
+                </a>
+              </p>
+            </div>
+
+        </div>
+        </div>
         </main>
       </div>
+
     )
   }
 }
